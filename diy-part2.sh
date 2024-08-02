eabb92c7cfec8cf134f45c78bdb9738ad6b357cb
dexxx-x <atmosfear85@gmail.com>
2024-08-03 00:06:23 +0500

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,10 +11,12 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/dexxx-usbmod/g' package/base-files/files/bin/config_generate
+
+sed -i 's/disabled/okay/g' /workdir/openwrt/target/linux/ramips/dts/mt7628an_xiaomi_mi-router-4c.dts
